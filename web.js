c5f0b3eef9c5ae312cda8b52242ef6ee807aa7c0
Iaroslav Babenko <iaroslav.babenko@gmail.com>
2013-07-10 19:11:47 +0000

    Correcte text output

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var outfile = "index.html";
+var text = fs.readfileSync(outfile);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text.toString());
 });
 
 var port = process.env.PORT || 5000;
